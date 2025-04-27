6a5be37c2f4a7132060c5c37e729dd204e3c9277
nano <nano@nanos-mbp.mynetworksettings.com>
2025-04-27 12:56:12 -0700

    Added files

---
+++ public/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+})
+
+// Route 1 - Climate Crisis
+app.get("/climate-crisis", (req, res) => {
+
+})
+
+// Route 2
+app.get("/typesetting", (req, res) => {
+
+})
+
+// Route 3
+app.get("/four-algorithms", (req, res) => {
+
+})
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:${PORT}');
+});
