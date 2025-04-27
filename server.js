c6042aed5a0c66f377567cbf3f44999ceeda5d1e
nano <nano@nanos-mbp.mynetworksettings.com>
2025-04-27 14:12:04 -0700

    Added express.static

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Root
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
